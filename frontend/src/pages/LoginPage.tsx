4047c8bd5aa2dd978a116509932860cbc059411f
ky heon <kuma@kys-MacBook-Air.local>
2023-06-22 18:49:39 -0700

    added event handlers for button clicks

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,20 @@
 
-import {FC} from "react"
+import { FC, MouseEvent } from "react"
 
 import {Form, Button} from "react-bootstrap"
 
 import HorizontilNavbar from "../components/HorizontilNavbar"
 import FormInput from "../components/FormInput"
-import TestComponent from "../components/TestComponent"
+import FormButton from "../components/FormButton"
+
 
 const LoginPage: FC = () => {
+  const onLoginBtnClick = (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
+		console.log(e.screenX, e.screenY)
+	}
+
+
   return (
     <div id="login-page-div" className="bg-gray-400 flex flex-col items-center justify-center">
       <HorizontilNavbar />
@@ -15,6 +22,8 @@ const LoginPage: FC = () => {
         <FormInput type="username" id="username-input" label="Username"></FormInput>
         <FormInput type="password" id="password-input" label="Password"></FormInput>
       </form>
+      <FormButton text="Login" callback={onLoginBtnClick}/>
+
     </div>
   )
 }
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,20 +1,30 @@
-import { FC } from "react"
+// node module imports
+import { FC, MouseEvent } from "react"
 
+// api imports
+
+// custom imports
 import FormButton from "../components/FormButton"
 import FormInput from "../components/FormInput"
 import HorizontilNavbar from "../components/HorizontilNavbar"
 
 const SignupPage: FC = () => {
 
+	const onSignupBtnClick = (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
+		console.log(e.screenX, e.screenY)
+	}
+
 	return (
-		<div id="signup-page-div" className="bg-gray-500 flex flex-col items-center">
+		<div id="signup-page-div" className="bg-gray-500 flex flex-col items-center justify-center">
 			<HorizontilNavbar />
+			
 			<form>
-				<FormInput type="email" id="email-input" label="email"/>
+				<FormInput type="email" id="email-input" label="Email"/>
 				<FormInput type="password" id="password1-input" label="Password"/>
 				<FormInput type="password" id="password2-input" label="Retype Password"/>
-				<FormButton />
 			</form>
+			<FormButton text="Sign Up" callback={onSignupBtnClick}/>
 		</div>
 	)
 }
